39c724b9a42ee560ca23dc44ab86107f7ab1da5d
sbelgard19 <belgard.scott@gmail.com>
2023-02-10 10:54:35 -0500

    Update MyFormButton.jsx

--- src/components/MyFormButton.jsx
+++ src/components/MyFormButton.jsx
@@ -4,10 +4,10 @@ function MyFormButton(props) {
 	return (
 		<div>
 			<form>
-				<label>Token Name</label>
-				<input size="100px" type="text" id="fname1" name="fname1"></input>
-				<label >Token Symbol</label>
-				<input size="20px" type="text" id="fname2" name="fname2"></input>
+				<label>Token Name:  </label>
+				<input size="25" type="text" id="fname1" name="TokenName"></input>
+				<label >Token Symbol:  </label>
+				<input size="3" maxlength="3" type="text" id="fname2" name="TokenSymbol"></input>
 			</form>
 			<button onClick={props.fcn} className="cta-button">
 				{props.buttonLabel}
