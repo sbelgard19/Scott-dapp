ca341b5824e609dc6bb56644734c29e23aae8d67
sbelgard19 <belgard.scott@gmail.com>
2023-02-10 10:30:27 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 
 import MyGroup from "./components/MyGroup.jsx";
+import MyFormGroup from "./components/MyFormGroup.jsx";
+
 import MyDoubleButton from "./components/MyDoubleButton.jsx";
 
 import walletConnectFcn from "./components/hedera/walletConnect.js";
