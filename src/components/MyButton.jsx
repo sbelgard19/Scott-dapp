58b7239fb78637ce63353269501536b033b56fbb
sbelgard19 <belgard.scott@gmail.com>
2023-02-10 10:03:07 -0500

    Update MyButton.jsx
    added text field

--- src/components/MyButton.jsx
+++ src/components/MyButton.jsx
@@ -3,6 +3,10 @@ import React from "react";
 function MyButton(props) {
 	return (
 		<div>
+			<form>
+				<label for="fname">Token Name</label>
+				<input type="text" id="fname" name="fname"></input>
+			</form>
 			<button onClick={props.fcn} className="cta-button">
 				{props.buttonLabel}
 			</button>
