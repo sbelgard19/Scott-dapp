9c7cf429e4c21dba6361fc227951718430066765
root <root@DESKTOP-PIP4NPJ>
2023-02-13 16:15:32 -0500

    added 4:11

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import tokenMintFcn from "./components/hedera/tokenMint.js";
 import contractDeployFcn from "./components/hedera/contractDeploy.js";
 import contractExecuteFcn from "./components/hedera/contractExecute.js";
 
- /* shortcut Ctrl + Shift + M  (problems)*/
+ /* shortcut Ctrl + Shift + M  (problems) 4:11*/
 
 import "./styles/App.css";
 
